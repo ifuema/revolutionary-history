537a9d2c81c117f1c1a6471488ba30df112cb95e
ifuema <y3bpcy3bpc@gmail.com>
2023-01-09 13:09:50 +0800

    初始化

---
+++ src/main/java/indi/revolutionaryhistory/RevolutionaryHistoryApplication.java
@@ -0,0 +1,13 @@
+package indi.revolutionaryhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutionaryHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevolutionaryHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indi/revolutionaryhistory/RevolutionaryHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.revolutionaryhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolutionaryHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
